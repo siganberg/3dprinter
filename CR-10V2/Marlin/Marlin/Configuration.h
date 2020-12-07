cb51b7c9e22a8d7957265f3ad8a0f6ba9b38d4ef
Francis <francis.marasigan@iherb.om>
2020-12-06 21:29:50 -0800

    Updated Marlin Configuration

--- CR-10V2/Marlin/Marlin/Configuration.h
+++ CR-10V2/Marlin/Marlin/Configuration.h
@@ -947,7 +947,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 48, 2, -2.5 }
+#define NOZZLE_TO_PROBE_OFFSET { 45, -13, -2.9 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1243,7 +1243,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
